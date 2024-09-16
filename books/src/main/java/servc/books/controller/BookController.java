4b13e1bbe2159724c0e5a157eaf05a2d61150813
Schneiderrrr <kat1906200152@gmail.com>
2024-09-16 12:25:48 +0300

    Controller refactoring

--- books/src/main/java/servc/books/controller/BookController.java
+++ books/src/main/java/servc/books/controller/BookController.java
@@ -1,13 +1,17 @@
 package servc.books.controller;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import servc.books.model.BookDTO;
 import servc.books.service.BookService;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@Validated
 @RequestMapping("/api/v1/books")
 public class BookController {
 
@@ -15,37 +19,37 @@ public class BookController {
     BookService bookService;
 
     @GetMapping
-    public List<BookDTO> GetAllBooks() {
+    public List<BookDTO> getAllBooks() {
         return bookService.GetAllBooks();
     }
 
     @GetMapping(params = {"take", "skip"})
-    public List<BookDTO> GetPartOfBooks(@RequestParam int take, @RequestParam int skip) {
+    public List<BookDTO> getPartOfBooks(@NotNull Integer take, @NotNull Integer skip) {
         return bookService.GetPartOfBooks(take, skip);
     }
 
     @GetMapping(params = {"sort"})
-    public List<BookDTO> GetSortedBooks(@RequestParam List<String> sort){
-        return bookService.GetSortedBooks(sort);
+    public List<BookDTO> getSortedBooks(@NotNull String[] sort){
+        return bookService.GetSortedBooks(Arrays.stream(sort).toList());
     }
 
     @GetMapping("/{id}")
-    public BookDTO GetBookById(@PathVariable Integer id){
+    public BookDTO getBookById(@PathVariable Integer id){
         return bookService.GetBookByID(id);
     }
 
     @PostMapping
-    public BookDTO AddBook(@RequestBody BookDTO book){
+    public BookDTO addBook(@RequestBody BookDTO book){
         return bookService.AddBook(book);
     }
 
     @PutMapping("/{id}")
-    public BookDTO UpdateBookByID(@PathVariable Integer id, @RequestBody BookDTO book){
+    public BookDTO updateBookByID(@PathVariable Integer id, @RequestBody BookDTO book){
         return bookService.UpdateBook(id, book);
     }
 
     @DeleteMapping("/{id}")
-    public void DeleteBookByID(@PathVariable Integer id){
+    public void deleteBookByID(@PathVariable Integer id){
         bookService.DeleteBookByID(id);
     }
 }
