df3885d327568bf8d513146452736851be2aadac
Katya Borovikova <kat1906200152@gmail.com>
2024-10-18 17:31:10 +0300

    Make books array to signal in list component

--- books-front/src/app/books/books.component.ts
+++ books-front/src/app/books/books.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { Book } from '../book';
 import { BookService } from '../book.service';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -10,14 +10,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  public books: Book[] = [];
+  public books = signal<Book[]>([]);
 
   public take?: number;
   public skip?: number;
   public sort?: string;
 
   constructor(
-    private bookService: BookService, 
+    private bookService: BookService,
     private route: ActivatedRoute
   ) {}
 
@@ -25,10 +25,10 @@ export class BooksComponent implements OnInit {
     this.take = Number(this.route.snapshot.paramMap.get('take'));
     this.skip = Number(this.route.snapshot.paramMap.get('skip'));
     this.sort = this.route.snapshot.paramMap.get('sort')?.toString();
-    
+
     if (this.take && this.skip){
       this.getPartOfBooks(this.take, this.skip);
-    } 
+    }
     else if (this.sort){
       this.getSortedBooks(this.sort);
     }
@@ -40,7 +40,7 @@ export class BooksComponent implements OnInit {
   public getBooks(): void{
     this.bookService.getBooks().subscribe(
       (response: Book[]) => {
-        this.books = response;
+        this.books.set(response);
       },
       (error: HttpErrorResponse) => {
         console.log(error.message);
@@ -48,11 +48,11 @@ export class BooksComponent implements OnInit {
       }
     );
   }
-  
+
   public getPartOfBooks(take: number, skip: number): void{
     this.bookService.getPartOfBooks(take, skip).subscribe(
       (response: Book[]) => {
-        this.books = response;
+        this.books.set(response);
       },
       (error: HttpErrorResponse) => {
         console.log(error.message);
@@ -60,11 +60,11 @@ export class BooksComponent implements OnInit {
       }
     );
   }
-  
+
   public getSortedBooks(sort: string): void{
     this.bookService.getSortedBooks(sort).subscribe(
       (response: Book[]) => {
-        this.books = response;
+        this.books.set(response);
       },
       (error: HttpErrorResponse) => {
         console.log(error.message);
@@ -74,15 +74,17 @@ export class BooksComponent implements OnInit {
   }
 
   public deleteBook(id: number): void{
-    this.books = this.books.filter(b => b.id != id);
-    this.bookService.deleteBook(id).subscribe();
+    this.bookService.deleteBook(id).subscribe(
+      () => {
+        this.books.update((books) => books.filter(b => b.id != id));
+      });
   }
 
   public addBook(name: string, isbn: string, author: string, date: Date ): void{
     var book = {name: name, isbn: isbn, author: author, releaseDate: date} as Book;
     this.bookService.addBook(book)
       .subscribe(book => {
-        this.books.push(book)
+        this.books.update((books) => [book, ...books]);
       });
   }
 }
